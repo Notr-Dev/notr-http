fb7e96df7770730e1688759a6aaf831fa8b75241
lazar <dasidasi86@gmail.com>
2024-09-20 10:08:31 +0300

    created the server

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package notrhttp
 
 import "net/http"
 
+type Server struct {
+	router *Router
+	port   string
+}
+
+func NewServer(port string) *Server {
+	if port[0] != ':' {
+		port = ":" + port
+	}
+	return &Server{
+		router: NewRouter(),
+		port:   port,
+	}
+}
+
 type Router struct {
 	mux http.ServeMux
 }
