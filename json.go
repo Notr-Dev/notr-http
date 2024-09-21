a01b74554e17d3ec4692fd06bf3e2e912c7c203a
lazar <dasidasi86@gmail.com>
2024-09-21 10:11:53 +0300

    Update json.go

--- json.go
+++ json.go
@@ -14,7 +14,11 @@ func (r *Request) GetJSONBody(v interface{}) error {
 }
 
 func (rw *Writer) RespondWithJson(code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
+	response, err := json.Marshal(payload)
+	if err != nil {
+		rw.RespondWithInternalError("Error marshalling response")
+		return
+	}
 	rw.Header().Set("Content-Type", "application/json")
 	rw.WriteHeader(code)
 	rw.Write(response)
