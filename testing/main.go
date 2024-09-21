6fb942d26181114a76f520167176f1ce8556cd0c
lazar <dasidasi86@gmail.com>
2024-09-21 10:34:12 +0300

    more complex testing code

--- testing/main.go
+++ testing/main.go
@@ -5,6 +5,28 @@ import notrhttp "github.com/Notr-Dev/notr-http"
 func main() {
 	server := notrhttp.NewServer("8080", "1.0")
 	server.SetName("My Server")
+	server.Post("/log", func(rw notrhttp.Writer, r *notrhttp.Request) {
+		type Response struct {
+			Password string `json:"password"`
+			Log      string `json:"log"`
+		}
+		var body Response
+		err := r.GetJSONBody(body)
+
+		if err != nil {
+			rw.RespondWithBadRequest("Invalid body")
+			return
+		}
+
+		if body.Password != "lazar" {
+			rw.RespondWithUnauthorized("Invalid password")
+			return
+		}
+
+		rw.RespondWithSuccess(map[string]interface{}{
+			"message": "Logged",
+		})
+	})
 	err := server.Run()
 	if err != nil {
 		panic(err)
