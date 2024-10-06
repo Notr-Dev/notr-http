5acde3b5ad23fc51830414544f83f361859cdcc3
lazar <dasidasi86@gmail.com>
2024-10-06 10:49:45 +0300

    Update static.go

--- static.go
+++ static.go
@@ -36,6 +36,33 @@ func (s *Server) ServeStatic(path string, dir string) {
 	)
 }
 
+func (s *Server) ServeStaticWebsite(path string, dir string) {
+	s.Routes = append(s.Routes,
+		Route{
+			Method: http.MethodGet,
+			Path:   path + "/{filename}",
+			Handler: func(rw Writer, r *Request) {
+				filename := r.Params["filename"]
+				filePath := filepath.Join(dir, filename)
+				fmt.Println("Serving file: ", filePath)
+
+				file, err := os.Open(filePath)
+				if err != nil {
+					http.Error(rw, "File not found", http.StatusNotFound)
+					return
+				}
+				defer file.Close()
+
+				rw.Header().Set("Content-Type", mime.TypeByExtension(filepath.Ext(filePath)))
+
+				if _, err := io.Copy(rw, file); err != nil {
+					http.Error(rw, "Error serving file", http.StatusInternalServerError)
+				}
+			},
+		},
+	)
+}
+
 func (s *Server) ServeHttpFileSystem(path string, fs http.FileSystem) {
 	s.Routes = append(s.Routes, Route{
 		Method: http.MethodGet,
